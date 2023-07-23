a0f82fe6d31dfddfe8bc9f39912e6ed85686b2c5
Veselov Nikolay <veselovnd@gmail.com>
2023-07-23 18:37:02 +0300

    5- liquibase, entity, docker

---
+++ notification-service/src/main/java/ru/veselov/notificationservice/entity/NotificationEntity.java
@@ -0,0 +1,44 @@
+package ru.veselov.notificationservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@EqualsAndHashCode(exclude = "notificationId")
+@Table(name = "notification")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NotificationEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "notification_id", columnDefinition = "uuid", updatable = false)
+    private UUID notificationId;
+
+    @Column(name = "customer_id")
+    private UUID customerId;
+
+    @Column(name = "to_customer_email")
+    private String toCustomerEmail;
+
+    @Column(name = "sender")
+    private String sender;
+
+    @Column(name = "message", columnDefinition = "varchar(1000")
+    private String message;
+
+    @Column(name = "sent_At")
+    private LocalDate sentAt;
+
+}
---
+++ notification-service/src/main/resources/db/changelog/changelogs/scripts/01-create-notification-table.sql
@@ -0,0 +1,16 @@
+CREATE
+    EXTENSION IF NOT EXISTS "uuid-ossp";
+
+GO
+
+create table notification
+(
+    notification_id   UUID          NOT NULL DEFAULT gen_random_uuid(),
+    customer_id       UUID          NOT NULL,
+    to_customer_email varchar       NOT NULL,
+    sender            varchar       NOT NULL,
+    message           varchar(1000) NOT NULL,
+    sent_at timestamp default now(),
+    CONSTRAINT notification_pk PRIMARY KEY (notification_id)
+)
+    GO
