f68155c716bc54870e1b7ac2907c0dfbae2d7ab0
Veselov Nikolay <veselovnd@gmail.com>
2023-07-27 21:05:26 +0300

    8-rabbit

--- notification-service/src/main/java/ru/veselov/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/ru/veselov/notificationservice/NotificationServiceApplication.java
@@ -1,13 +1,30 @@
 package ru.veselov.notificationservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.veselov.amqp.producer.RabbitMQMessageProducer;
+import ru.veselov.notificationservice.config.NotificationConfig;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "ru.veselov.notificationservice",
+                "ru.veselov.amqp"
+        }
+)
 public class NotificationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,
+                                               NotificationConfig notificationConfig) {
+        return args -> producer.publish("test",
+                notificationConfig.getInternalExchange(),
+                notificationConfig.getInternalNotificationRoutingKey());
+    }
 
 }
